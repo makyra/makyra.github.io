d379da7950bd05cf7a318276c3ba28f4d5b140c6
makyra <52718767+makyra@users.noreply.github.com>
2019-07-10 11:46:29 -0500

    Add files via upload

---
+++ website/makesite.js
@@ -0,0 +1,19 @@
+function bigger(x) {
+  x.style.height = "30%";
+  x.style.width = "30%";
+}
+
+function normal(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
+
+"bells" : {
+  url : "bells.wav"
+  volume : .30
+}
+
+"hohoho" : {
+  url : "hohoho.wav",
+  volume : .30
+}
